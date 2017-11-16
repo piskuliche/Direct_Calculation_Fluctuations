e9c76c202ff9acc2c96b3dcab10dd8cac57b41ea
Piskulich <piskuliche@ku.edu>
2017-11-16 15:52:50 -0600

    Initialized Directory

---
+++ backbone.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+#This is a backbone script that should run all of the important steps of the analysis
---
+++ cleanup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This is a utility to clean up after a use of the energy fluctuations method.
+
+
+
