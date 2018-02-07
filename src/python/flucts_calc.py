4f932a027d5c476dc17aa993b2314c1b95c351c9
Zeke Piskulich <piskuliche@ku.edu>
2018-02-07 15:03:12 -0600

    Updated the DCM code to include higher order derivatives - test phase only

--- src/python/flucts_calc.py
+++ src/python/flucts_calc.py
@@ -22,6 +22,9 @@ nblocks = int(args.blocks)
 ntimes = int(args.ntimes)
 mol_name = str(args.mol)
 
+# Define Corr-Array
+corr_funcs=['msd','c1','c2']
+
 # Read in input file
 inp_names, inp_cols=np.genfromtxt(inputfile, usecols=(0,1), dtype=None,unpack=True)
 
@@ -29,170 +32,162 @@ inp_names, inp_cols=np.genfromtxt(inputfile, usecols=(0,1), dtype=None,unpack=Tr
 filesperblock=nfiles/float(nblocks)
 t_val=stats.t.ppf(0.95,nblocks-1)/np.sqrt(nblocks)
 
-# Initialize Vectors
-msd=[ntimes]
-c2=[ntimes]
-ival=[nfiles]
-dival=[nfiles]
-d2ival=[nfiles]
+time = []
+for i in range(0, ntimes):
+    time.append(i*0.05)
 
-# First Derivatives
-divalmsd=[ntimes]
-divalc1=[ntimes]
-divalc2=[ntimes]
+for corr_name in corr_funcs:
+    # Initialize Vectors
+    cab=[ntimes]        # Unweighted Corr func
+    fval1=[nfiles]      # Fluct Value 1
+    fval2=[nfiles]      # Fluct Value 2
+    dfval1=[nfiles]     # Weighted Fluct Correlation Function 1
+    dfval2=[nfiles]     # Weighted Fluct Correlation Function 2
+    
+    # First Derivatives
+    dfvalcab=[ntimes]
+    
+    # Second Derivatives
+    d2fvalcab=[ntimes]
+    
+    # Block Initializations
+    eafval_bl[[] for x in range(0,nblocks)]
+    cab_bl[[] for x in range(0,nblocks)]
+    dcabfval_bl=[[] for x in range(0,nblocks)]
+    
+    for i in range(0,nfiles):
+        dfval1.append(0)
+        dfval2.append(0)
 
-# Second Derivatives
-d2ivalmsd=[ntimes]
-d2ivalc1=[ntimes]
-d2ivalc2=[ntimes]
+    # Read in File Names
+    fnames=[np.genfromtxt('file_names', dtype='string', unpack=True)]
+    fcab=["FILES/"+s+"/"+corr_name+"_"+s+"_"+mol_name+".dat" for s in fnames]
+    item_count=0
+    # Do the Block Average Calculation
+    for item1 in inp_names:
+        print item1
+        fval1 = np.genfromtxt(item1+'_init.out',unpack=True) # Reads in the values of the energy
+        for item2 in inp_names:
+            if inp_names.index[item2] >= item_count:
+                print item2
+                fval2 = np.genfromtxt(item2+'_init.out', unpack=True) # Reads in the values of the energy
+                for block in range(0,nblocks):
+                    start=int(block*filesperblock)
+                    end=int((block+1)*filesperblock)
+                    print start,end
+                    fval1avg_bl = np.average(fval1[start:end])
+                    fval2avg_bl = np.average(fval2[start:end])
+                    for j in range(start,end):
+                        dfval1[j]=fval1[j]-fval1avg_bl
+                        dfval2[j]=fval2[j]-fval2avg_bl
+                        d2fval[j]=dfval1[j]*dfval2[j]
+                    d2fvalavg_bl = np.average(d2fval)
+                    for j in range(0,ntimes):
+                        cab[j]=0
+                        dfvalcab[j]=0
+                        d2fvalcab[j]=0
+                        d2fvalavcab[j]=0
+                    for j in range(start,end):
+                        tcab=np.genfromtxt(fcab[j], usecols=1,unpack=True)
+                        # Weight by Fluct
+                        cab=cab+tcab
+                        dfvalcab=dfvalcab+tcab*dfval[j]
+                        d2fvalcab=d2fvalcab+tcab*d2fval[j]
+                        d2fvalavcab=d2fvalavcab+tcab*d2fvalavg_bl
+                    # Normalize
+                    cab[:] = [x / float(filesperblock) for x in cab]
+                    dfvalcab[:] = [x / float(filesperblock) for x in dfvalcab]
+                    d2fvalcab[:] = [x / float(filesperblock) for x in d2fvalcab]
+                    d2fvalavcab[:] = [x / float(filesperblock) for x in d2fvalavcab]
+                    # Calculate the Ratio of Weighted to Unweighted Corr Functions
+                    eafval=dfval[1:]/cab[1:]
+                    # Save to list
+                    eafval_bl[block] = list(eafval)
+                    # Save correlation info to sep list
+                    cab_bl[block]=list(cab)
+                    dcabfval_bl[block]=list(dfvalcab[1:])
+                    d2cabfval_bl[block]=list(d2fvalcab[1:])
+                    d2cabavfval_bl[block]=list(d2fvalavcab[1:])
+                    # Print Single Correlation Values
+                    np.savetxt("bl_"+str(block)+"_"+item+"_"+mol_name+"_"+corr_name+".dat", np.c_[time[1:], cab[1:], dfvalcab[1:],eafval], fmt='%s')
+                    # Print Second Derivatives
+                    np.savetxt("bl_"+str(block)+"_"+item1+"_"+item2+"_"+mol_name+"_"+corr_name+".dat", np.c_[time[1:],d2fvalcab[1:], d2fvalavcab[1:], d2fvalcab[1:]-d2fvalavcab[1:])
+                    # Zero the items
+                    for i in range(start,end):
+                        dfval1[i]=0
+                        dfval2[i]=0
+                    for i in range(0,ntimes):
+                        cab[i]=0
+                        dfvalcab[i]=0
+                        d2fvalcab[i]=0
+                        d2fvalavcab[i]=0
+                    for i in range(0,ntimes-1):
+                        eafval[i]=0
+        item_count+=1
+    # This Ends the Block Calculation
+    # Do the total calculation
+    item_count = 0
+    for item1 in inp_names:
+        print item1
+        fval1 = np.genfromtxt(item1+"_init.out", unpack=True) # Reads in Values for the Energy
+        for item2 in inp_names:
+            if inp_names.index[item2] >= item_count:
+                print item 2
+                fval2 = np.genfromtxt(item2+'_init.out', unpack=True) # Reads int he values of the energy
+                fval1avg = np.average(fval1)
+                fval2avg = np.average(fval2)
+                for i in range(0, nfiles):
+                    dfval1[j]=fval1[j]-fval1avg
+                    dfval2[j]=fval2[j]-fval2avg
+                    d2fval[j]=dfval1[j]*dfval2[j]
+                d2fvalavg = np.average(d2fval)
+                for j in range(0,ntimes):
+                    cab[j]=0
+                    dfvalcab[j]=0
+                    d2fvalcab[j]=0
+                    d2fvalavcab[j]=0
+                for j in range(0,nfiles):
+                    tcab=np.genfromtxt(fcab[j],usecols=1, unpack=True)
+                    # Weight by flucts
+                    cab=cab+tcab
+                    dfvalcab=dfvalcab+tcab*dfval[j]
+                    d2fvalcab=d2fvalcab+tcab*d2fval[j]
+                    d2fvalavcab=d2fvalavcab+tcab*d2fvalavg
+                # Normalize
+                cab[:] = [x / float(nfiles) for x in cab]
+                dfvalcab[:] = [x / float(nfiles) for x in dfvalcab]
+                d2fvalcab[:] = [x / float(nfiles) for x in d2fvalcab]
+                d2fvalavcab[:] = [x / float(nfiles) for x in d2fvalavcab]
+                # Calculate the Ratio of Weighted to Unweighted Corr Functions
+                eafval=dfval[1:]/cab[1:]
 
-eaival_bl=[[] for x in range(0,nblocks)] 
-msd_bl=[[] for x in range(0,nblocks)]
-dmsdival_bl=[[] for x in range(0,nblocks)]
-d2msdival_bl=[[] for x in range(0,nblocks)]
-dc2ival_bl=[[] for x in range(0,nblocks)]
-d2c2ival_bl=[[] for x in range(0,nblocks)]
-c2_bl=[[] for x in range(0,nblocks)]
+                # Calculate Uncertainty
+                err_cab = np.array(cab_bl).std(0)
+                err_fvalcab = np.array(eafval_bl).std(0)
+                err_dfvalcab = np.array(dfvalcab_bl).std(0)
+                err_d2fvalcab = np.array(dfvalcab_bl).std(0)
+                err_d2fvalavcab = np.array(dfvalavcab_bl).std(0)
+                
+                err_cab = [x * t_val for x in err_cab]
+                err_fvalcab = [x * t_val for x in err_fvalcab]
+                err_dfvalcab = [x * t_val for x in err_dfvalcab]
+                err_d2fvalcab = [x * t_val for x in err_d2fvalcab]
+                err_d2fvalavcab = [x * t_val for x in err_d2fvalavcab]
 
-for i in range(0,nfiles):
-    dival.append(0)
-    d2ival.append(0)
+                # Print Output
+                np.savetxt(item1+"_"+mol_name+"_"+corr_name+".dat", np.c_[time[1:], cab[1:], err_cab[1:], dfvalcab[1:],err_dfvalcab[1:], eafval,err_fvalcab], fmt='%s')
+                np.savetxt(item1+"_"+item_2+"_"+mol_name+"_"+corr_name+".dat", np.c_[time[1:], d2fvalcab[1:], err_d2fvalcab[1:], d2fvalavcab[1:],err_d2fvalavcab[1:], d2fvalcab[1:]-d2fvalavcab[1:], err_d2fvalcab[1:]-err_d2fvalavcab[1:]], fmt='%s')
+                # Zero Items
+                for i in range(0,nfiles):
+                    dfval1[i]=0
+                    dfval2[i]=0
+                for i in range(0,ntimes):
+                    cab[i]=0
+                    dfvalcab[i]=0
+                    d2fvalcab[i]=0
+                    d2fvalavcab[i]=0
+                for i in range(0,ntimes-1):
+                    eafval[i]=0
+        item_count += 1
 
-# Read in File Names
-fnames=np.genfromtxt('file_names', dtype='string', unpack=True)
-fmsd=["FILES/"+s+"/msd_"+s+'_'+mol_name+'.dat' for s in fnames]
-fc1=["FILES/"+s+"/c1_"+s+'_'+mol_name+".dat" for s in fnames]
-fc2=["FILES/"+s+"/c2_"+s+'_'+mol_name+".dat" for s in fnames]
-
-# Do the block average calculation
-# item in this loop is like energy, volume, etc.
-for item in range(0,len(inp_names)):
-    print inp_names[item] # prints the name of the ival
-    ival=np.genfromtxt(inp_names[item]+'_init.out', unpack=True) # Reads in the values of the energy.
-    for block in range(0,nblocks): # loop over the blocks
-        start=int(block*filesperblock)
-        end=int((block+1)*filesperblock)
-        print start,end
-        ivalavg_bl=np.average(ival[start:end])
-        for j in range(start,end): # calculates the difference from the average
-            dival[j]=ival[j]-ivalavg_bl
-            d2ival[j]=pow(dival[j],2)
-        d2ivalav=np.average(d2ival[start:end])
-        for j in range(0, len(msd)):
-            msd[j]=0
-            divalmsd[j]=0
-            c2[j]=0
-            divalc2[j]=0
-        for j in range(start,end):
-            tmsd=np.genfromtxt(fmsd[j], usecols=1, unpack=True)
-            tc2=np.genfromtxt(fc2[j], usecols=1, unpack=True)
-            # MSD Weighted by Fluct
-            msd=msd+tmsd
-            divalmsd=divalmsd+tmsd*dival[int(j)]
-            d2ivalmsd=divalmsd+tmsd*(d2ival[int(j)]-d2ivalav)
-            # C2 Weighted by Fluct
-            c2=c2+tc2
-            divalc2=divalc2+tc2*dival[int(j)]
-            d2ivalc2=divalc2+tc2*(d2ival[int(j)]-d2ivalav)
-        # Normalize the lists
-        # MSD
-        msd[:] = [x / float(filesperblock) for x in msd]
-        divalmsd[:] = [x / float(filesperblock) for x in divalmsd]
-        d2ivalmsd[:] = [x / float(filesperblock) for x in d2ivalmsd]
-        # C2
-        c2[:] = [x / float(filesperblock) for x in c2]
-        divalc2[:] = [x / float(filesperblock) for x in divalc2]
-        d2ivalc2[:] = [x / float(filesperblock) for x in d2ivalc2]
-        # Calculate Activation Energies
-        eaival=divalmsd[1:]/msd[1:]
-        # Save to Sep List
-        eaival_bl[block] = list(eaival)
-        # Saves msd_info to sep list
-        msd_bl[block]=list(msd)
-        dmsdival_bl[block]=list(divalmsd[1:])
-        d2msdival_bl[block]=list(d2ivalmsd[1:])
-        # Saves c2 to sep list
-        c2_bl[block]=list(c2)
-        dc2ival_bl[block]=list(divalc2)
-        d2c2ival_bl[block]=list(d2ivalc2)
-        # print block values
-        np.savetxt('bl_'+str(block)+'_'+inp_names[item]+'_'+mol_name+'_msd.dat', np.c_[eaival], fmt='%s')
-        np.savetxt('bl_'+str(block)+'_'+inp_names[item]+'_'+mol_name+'_c2.dat', np.c_[divalc2], fmt='%s')
-        # Zero the items in the range
-        for i in range(start,end):
-            dival[i]=0
-        for i in range(0,len(msd)):
-            msd[i]=0
-            divalmsd[i]=0
-            d2ivalmsd[i]=0
-        for k in range(0,len(eaival)):
-            eaival[k]=0
-    ivalavg=np.average(ival)
-    print ivalavg
-    # Zeroes the msd vectors and recalculates the d_values
-    for j in range(0,nfiles):
-        dival[j]=ival[j]-ivalavg
-        d2ival[j]=pow(dival[j],2)
-    d2ivalav=np.average(d2ival)
-    for j in range(0,len(msd)):
-        msd[j]=0
-        divalmsd[j]=0
-        d2ivalmsd[j]=0
-        c2[j]=0
-        divalc2[j]=0
-        d2ivalc2[j]=0
-    for j in range(0,nfiles):
-        tmsd=np.genfromtxt(fmsd[j], usecols=1, unpack=True)
-        tc2=np.genfromtxt(fc2[j], usecols=1, unpack=True)
-        # Weighted By Flucts
-        # MSD
-        msd=msd+tmsd
-        divalmsd=divalmsd+tmsd*dival[int(j)]
-        d2ivalmsd=divalmsd+tmsd*(d2ival[int(j)]-d2ivalav)
-        # C2
-        c2=c2+tc2
-        divalc2=divalc2+tc2*dival[int(j)]
-        d2ivalc2=divalc2+tc2*(d2ival[int(j)]-d2ivalav)
-    # Normalize Results
-    msd[:] = [x / float(nfiles) for x in msd]
-    divalmsd[:] = [x / float(nfiles) for x in divalmsd]
-    d2ivalmsd[:] = [x / float(nfiles) for x in d2ivalmsd]
-    c2[:] = [x / float(nfiles) for x in c2]
-    divalc2 = [x / float(nfiles) for x in divalc2]
-    d2ivalc2 = [x / float(nfiles) for x in d2ivalc2]
-    eaival=divalmsd[1:]/msd[1:]
-    # Save Data
-    np.savetxt('d'+inp_names[item]+'_'+mol_name+'.dat', dival, fmt=['%.4f'])
-    np.savetxt('d2'+inp_names[item]+'_'+mol_name+'-<'+inp_names[item]+'>.dat',d2ival-d2ivalav, fmt=['%.4f'])
-    # Calculate Uncertainty
-    # MSD
-    err_ivalmsdea=np.array(eaival_bl).std(0)
-    err_ivalmsdea= [x * t_val for x in err_ivalmsdea]
-    # DMSD
-    err_divalmsd=np.array(dmsdival_bl).std(0)
-    err_divalmsd= [x * t_val for x in err_divalmsd]
-    err_d2ivalmsd=np.array(d2msdival_bl).std(0)
-    err_d2ivalmsd= [x * t_val for x  in err_d2ivalmsd]
-
-    # DC2
-    err_dc2ival=np.array(dc2ival_bl).std(0)
-    err_dc2ival= [x * t_val for x in err_dc2ival]
-    err_d2c2ival=np.array(d2c2ival_bl).std(0)
-    err_d2c2ival= [x * t_val for x in err_d2c2ival]
-
-    # Calculate Time
-    time = []
-    for i in range(0, len(err_ivalmsdea)+1):
-        time.append(i*0.05)
-
-    np.savetxt('ea_msd_'+inp_names[item]+'_'+mol_name+'.dat', np.c_[time[1:], eaival, err_ivalmsdea], fmt='%s')
-    np.savetxt('d'+inp_names[item]+'_'+mol_name+'_msd_tot.dat', np.c_[time[1:], divalmsd[1:], err_divalmsd], fmt='%s')
-    np.savetxt('d2'+inp_names[item]+'_'+mol_name+'_msd_tot.dat', np.c_[time[1:], d2ivalmsd[1:], err_d2ivalmsd], fmt='%s')
-    np.savetxt('dc2_'+inp_names[item]+'_'+mol_name+'.dat', np.c_[time, divalc2, err_dc2ival], fmt='%s')
-    np.savetxt('d2c2_'+inp_names[item]+'_'+mol_name+'.dat', np.c_[time, d2ivalc2, err_d2c2ival], fmt='%s')
-    np.savetxt('c2_total'+'_'+mol_name+'_result.dat', np.c_[time,c2], fmt='%s')
-    np.savetxt('time.dat',time,fmt='%s')
-    for i in range(0, nblocks):
-        np.savetxt('bl_'+str(int(i))+'_'+mol_name+'_c2_val.dat', c2_bl[i], fmt='%s')
-        np.savetxt('bl_'+str(int(i))+'_'+mol_name+'_dc2_val_'+inp_names[item]+'.dat', dc2ival_bl[i], fmt='%s')
-
+# Program Complete
