5ad1579b2b97cc7e0ce9d2064be2387a39c3cf90
Zeke Piskulich <piskuliche@ku.edu>
2018-03-12 16:06:39 -0500

    added viscosity corr func

---
+++ src/fortran/visc_calc.f90
@@ -0,0 +1,66 @@
+! This is a program to calculate the shear and bulk viscosity
+
+Program visc_calc
+
+      implicit none
+      integer :: i, t
+      integer :: ntimes
+      real :: dt, volume
+      
+      real :: pxxo, pyyo, pzzo
+      real :: pxyo, pxzo, pyzo
+      real :: p_xyo, p_yzo
+
+      real :: Pxy, Pxz, Pyz
+      real :: P_xy, P_yz
+
+      real, dimension(3) :: L
+      real, dimension(6) :: press
+      real, dimension(5000) :: P_av
+
+      character(len=10) nfile
+
+      open(10,file='msd_rot_calc.in',status='old')
+
+      read(10,*)
+      read(10,*) nfile
+      read(10,*) 
+      read(10,*) ntimes, dt
+      read(10,*)
+      read(10,*) volume
+      close(10)
+
+      L(1) = volume ** (1.0/3.0)
+      L(2) = L(1)
+      L(3) = L(1)
+      
+      open(11, file='pressures_out.log',status='old')
+      
+      ! Zero the results for NVE trajectory
+      pxxo = 0.0; pyyo = 0.0; pzzo = 0.0
+      pxyo = 0.0; pxzo = 0.0; pyzo = 0.0
+      p_xyo = 0.0; p_yzo = 0.0
+      
+      read(11,*) itmp, pxxo, pyyo, pzzo, pxyo, pxzo, pyzo
+      p_xyo = (pxxo-pyyo)/2.0
+      p_yzo = (pyyo-pzzo)/2.0
+      P_av(0) = (pxyo + pxzo + pyzo + p_xyo + p_yzo)/5.0 
+
+      ! Loops over times
+      do t = 1, ntimes
+           read(11,*) itmp, (press(k),k=1,6) 
+           Pxy = pxyo * press(4) 
+           Pxz = pxzo * press(5) 
+           Pyz = pyzo * press(6)
+           P_xy = p_xyo * (press(1) - press(2))/2.0
+           P_yz = p_yzo * (press(2) - press(3))/2.0
+           P_av(t) = (Pxy + Pxz + Pyz + P_xy + P_yz)/5.0
+      end do
+      
+      open(15, file='shear_'//trim(nfile)//'_'//trim(mol_name)//'.dat')
+      do t = 1, ntimes
+        write (15,'(2F12.5)') dt*i, P_av(t)
+      end do
+
+
+end program visc_calc
