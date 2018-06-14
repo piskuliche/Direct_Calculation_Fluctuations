d17b899dc0739bd1f55111f51d2dbe82d1c306d5
Zeke Piskulich <piskuliche@ku.edu>
2018-06-14 16:34:43 -0500

    Updated grab flucts

--- src/python/grab_flucts.py
+++ src/python/grab_flucts.py
@@ -1,53 +1,53 @@
-#grab_flucts.py is the program that parses each log file and grabs each of the energies/volumes/whatever thermodynamic data you want to weight the correlation functions
-#required input: log.lammps (for each directory)
-#produced ouptut: *_init.out 
+#grab_flucts.py is the program that parses each log file and grabs each of the energies/volumes/whatever thermodynamic data you want to weight the correlation fucntions. 
+#   required input: either log.lammps or flucts.ener
+#   produced output: *_init.out
 
 import numpy as np
-import argparse
-from argparse import RawTextHelpFormatter
+import sys
+from read_input import input
 
-# Read in Arguments
-parser = argparse.ArgumentParser(description='''Calculates the fluctuations of the NPT Ensemble''', formatter_class=RawTextHelpFormatter)
-parser.add_argument('-inp', help="Input File")
-parser.add_argument('-ind', help = "If you want a single element - type its position in the input file (counting from 0). If you want the whole thing - type FALSE")
-args = parser.parse_args()
+value = int(sys.argv[1])
+
+inputparam = input('input_file')
 
 filenames='file_names'
 
-input_file=str(args.inp)
-value=int(args.ind)
-avg=[]
-fnames=[]
-
-
-
-# Read Input File
-ivalname, icolnum = np.genfromtxt(input_file, usecols=(0,1), dtype=(str,int), unpack=True)
-
-print ivalname[0]
-print icolnum[0]
-
-# Loop over types of values, e, v, ke, etc
+ivalname, icolnum = np.genfromtxt(input_file, usecols(0,1), dtype=(str,int), unpack=True)
 ival_list = np.array([])
-with open(filenames) as f:
-    for l in f:
-        filename='FILES/'+l.rstrip()+'/log.lammps'
-        print filename
-        lookup = 'Step'
-        lookup2 = 'Loop time'
-        totlines = 0
-        with open(filename) as myFile:
-            for num, line in enumerate(myFile, 1):
-                if lookup in line:
-                    startskip=num
-                if lookup2 in line:
-                    endskip=num
-                totlines=num
-        endskip=totlines-endskip-4
-        ival = np.genfromtxt(filename, skip_header=startskip, skip_footer=endskip, usecols=(int(icolnum[value])-1), unpack=True)
-        ival_list = np.append(ival_list, ival[0])
 
-fileout=str(ivalname[value])+"_init.out"
-np.savetxt(fileout, ival_list, fmt=['%.4f'])
+if inputparam.prog == 'LAMMPS':
+    # Looks for log.lammps
+    with open(filenames) as f:
+        for l in f:
+            filename='FILES/'+l.rstrip()+'/log.lammps'
+            lookup='Step'
+            lookup2='Loop time'
+            totlines = 0
+            with open(filename) as myFile:
+                for num, line in enumerate(myFile,1):
+                    if lookup in line:
+                        startskip=num
+                    if lookup2 in line:
+                        endskip=num
+                    totlines=num
+            endskip=totlines-endskip-4
+            ival = np.genfromtxt(filename, skip_header=startskip, skip_footer=endskip, usecols=(int(icolnum[value])-1), unpack=True)
+            ival_list = np.append(ival_list, ival[0])
+    fileout=str(ivalname[value])+"_init.out"
+    np.savetxt(fileout, ival_list, fmt=['%.4f'])
+else if inputparam.prog == 'CP2K':
+    # Looks for CP2K Output File
+    with open(filenames) as f:
+        for l in f:
+            filename='FILES/'+l.rstrip()+'flucts.ener'
+            with open(filename) as myFile:
+                myFile.readline()
+                ival=(myFile.readline().strip())[int(icolnum[value])-1], unpack=True)
+                ival_list = np.append(ival_list, ival)
+    fileout=str(ivalname[value])+"_init.out"
+    np.savetxt(fileout, ival_list, fmt=['%.4f'])
 
-        
+            
+else:
+    print("%s is not a valid program" % inputparam.prog)
+
