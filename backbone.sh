f9fa1e8f22430b48349eeefd06bce8f3846b931e
Zeke Piskulich <piskuliche@ku.edu>
2018-07-09 17:41:08 -0500

    updated backbone so it no longer overwrites input_file

--- backbone.sh
+++ backbone.sh
@@ -16,9 +16,8 @@ if [ -f $FILE ]; then
     echo "-Instructions Flag Exists"
 else
     echo "-Instructions Flag Doesn't Exist"
-    echo "-Please modify input_file"
-    cp src/input/input_file ../
-    cp src/input/flucts.inp ../
+    echo "-Please create an input_file, one is included in src/dependencies"
+    echo "-Please create an flucts.inp, one is included in src/dependencies"
     touch .flag_instruct
     exit 0
 fi
